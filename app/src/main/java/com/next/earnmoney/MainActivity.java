54ec478f6141a6ca63a9987646ff145266b36bbf
AMRARAM <amramanoj@gmail.com>
2022-01-25 06:50:36 +0530

    msg

---
+++ app/src/main/java/com/next/earnmoney/MainActivity.java
@@ -0,0 +1,17 @@
+package com.next.earnmoney;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+CardView crd;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
